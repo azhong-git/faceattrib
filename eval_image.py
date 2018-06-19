ce76ab685528c6a54ac028267e6149b87c296eef
azhongwl <azhong@AF051506.employee-wifi.rwc.corp.imvu.com>
2018-06-19 15:30:55 -0700

    clean demo scripts

--- eval_image.py
+++ eval_image.py
@@ -4,11 +4,12 @@ import numpy as np
 
 face = cv2.imread(sys.argv[1])
 face = cv2.resize(face, (224, 224))
+face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
 face = np.array(face)
 face = (face / 255.0 - 0.5) * 2.0
 faces = np.expand_dims(face, axis=0)
 
-from labels import LABELS
+from labels import LABELS_40
 import keras
 from keras.models import load_model
 # definte relu6
@@ -16,10 +17,10 @@ from tensorflow.python.keras import backend as K
 def relu6(x):
     return K.relu(x, max_value=6)
 
-model = load_model('models/mobilenet_1.0_224_2018_06_13_17_27_59/face_attrib_mobilenet_1.0_224.25-0.32-0.08.hdf5',
+model = load_model('models/mobilenet_1.0_224_2018_06_18_16_08_08/face_attrib_mobilenet_1.0_224.10-0.18-0.17.hdf5',
                    custom_objects={'relu6': relu6})
 val_predicted = model.predict(faces)
 
 sorted_inds = np.argsort(val_predicted[0])[::-1]
-for i in range(len(LABELS)):
-    print('{}, {:.02f}'.format(LABELS[sorted_inds[i]], val_predicted[0][sorted_inds[i]]))
+for i in range(len(LABELS_40)):
+    print('{}, {:.02f}'.format(LABELS_40[sorted_inds[i]], val_predicted[0][sorted_inds[i]]))
--- eval_video.py
+++ eval_video.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 face_detector = cv2.CascadeClassifier('models/haarcascade_frontalface_default.xml')
 
 cv2.namedWindow('window_frame')
@@ -8,7 +9,7 @@ w_ext_ratio = [0.3, 0.3]
 h_ext_ratio = [0.4, 0.20]
 take_selfie = False
 
-from labels import LABELS
+from labels import LABELS_40
 import keras
 from keras.models import load_model
 # definte relu6
@@ -16,12 +17,13 @@ from tensorflow.python.keras import backend as K
 def relu6(x):
     return K.relu(x, max_value=6)
 
-model = load_model('models/mobilenet_1.0_224_2018_06_13_17_27_59/face_attrib_mobilenet_1.0_224.25-0.32-0.08.hdf5',
+model = load_model('models/mobilenet_1.0_224_2018_06_18_16_08_08/face_attrib_mobilenet_1.0_224.10-0.18-0.17.hdf5',
                    custom_objects={'relu6': relu6})
 
 while True:
     bgr_image = video_capture.read()[1]
     gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+    rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
     width = bgr_image.shape[1]
     height = bgr_image.shape[0]
     if take_selfie:
@@ -43,24 +45,26 @@ while True:
         y_from_out = max(h_ext[0]-y, 0)
         x_to = min(x+w+w_ext[1], width - 1)
         y_to = min(y+h+h_ext[1], height - 1)
-        face_crop[y_from_out:(y_to-y_from+y_from_out), x_from_out:(x_to-x_from+x_from_out)] = bgr_image[y_from:y_to, x_from:x_to]
+        face_crop[y_from_out:(y_to-y_from+y_from_out), x_from_out:(x_to-x_from+x_from_out)] = rgb_image[y_from:y_to, x_from:x_to]
         face_crop = face_crop.astype(np.uint8)
         face_crop = cv2.resize(face_crop, (224, 224))
         face_in = np.array(face_crop)
         face_in = (face_in / 255.0 - 0.5) * 2.0
         face_in = np.expand_dims(face_in, axis=0)
+        start = time.time()
         val_predicted = model.predict(face_in)
+        print('One frame took {:.2f} ms'.format((time.time()-start)*1e3))
 
         sorted_inds = np.argsort(val_predicted[0])[::-1]
         cv2.rectangle(bgr_image, (x,y), (x+w, y+h), (255, 255, 255), 2)
-        cv2.imshow('face_crop', face_crop)
+        face_crop_bgr = cv2.cvtColor(face_crop, cv2.COLOR_RGB2BGR)
+
+        cv2.imshow('face_crop', face_crop_bgr)
         print('---------------------------------------------------------------------------------')
-        for i in range(len(LABELS)):
-            print('{}, {:.02f}'.format(LABELS[sorted_inds[i]], val_predicted[0][sorted_inds[i]]))
+        for i in range(len(LABELS_40)):
+            print('{}, {:.02f}'.format(LABELS_40[sorted_inds[i]], val_predicted[0][sorted_inds[i]]))
         take_selfie = False
 
     cv2.imshow('window_frame', bgr_image)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
     if cv2.waitKey(1) & 0xFF == ord('c'):
         take_selfie = True
