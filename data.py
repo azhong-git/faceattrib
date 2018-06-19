ac2b4be2cb7ed08b4deeaaf31e26a29d64f6504a
Andrew Zhong <azhong@imvu.com>
2018-06-18 17:49:23 -0700

    use keras generator to train on multi-label, some local hacks to keras's preprocessing library

--- data.py
+++ data.py
@@ -4,27 +4,47 @@ import cv2
 import numpy as np
 import pickle
 
-def load_lfw_data(image_dir, attributes_path, input_shape = (224, 224, 3), random=True, limit = -1):
+def load_attributes(attributes_path):
+  keys_lines = pickle.load(open(attributes_path, 'rb'))
+  keys = keys_lines['header']
+  labels = keys[1:]
+  lines = keys_lines['lines']
+  multilabel_dict = {}
+  for line in lines:
+    multilabel_dict[line[0]] = np.array(line[1:])
+  return multilabel_dict, labels
+
+
+def load_data(image_dir, attributes_path, input_shape = (224, 224, 3), crop_middle=True, random=False, limit = -1):
   assert os.path.isfile(attributes_path), "Annotation '" + attributes_path + "' not found."
   keys_lines = pickle.load(open(attributes_path, 'rb'))
   keys = keys_lines['header']
   lines = keys_lines['lines']
-  labels = keys[2:]
+  labels = keys[1:]
   if random:
       np.random.shuffle(lines)
-
   faces = []
   attributes = []
   count = 0
   for line in lines:
-      image_path = os.path.join(image_dir, ('_'.join(line[0].split())))
-      image_path += '_{:04}.jpg'.format(int(line[1]))
+      image_path = os.path.join(image_dir, line[0])
       assert os.path.isfile(image_path), '{} not found'.format(image_path)
-      face = cv2.imread(image_path)
-      face = cv2.resize(face, input_shape[:2])
-      faces.append(face.astype('float32'))
-      attributes.append(line[2:])
+      bgr_face = cv2.imread(image_path)
+      width = bgr_face.shape[1]
+      height = bgr_face.shape[0]
+      if width != height and crop_middle == True:
+        if height > width:
+          start = int((height - width) / 2)
+          bgr_face = bgr_face[start:(height-start), 0:width, 0:3]
+        else:
+          start = int((width - height) / 2)
+          bgr_face = bgr_face[0:height, start:(width-start), 0:3]
+      bgr_face = cv2.resize(bgr_face, input_shape[:2])
+      rgb_face = cv2.cvtColor(bgr_face, cv2.COLOR_BGR2RGB)
+      faces.append(rgb_face.astype('float32'))
+      attributes.append(line[1:])
       count += 1
+      # print('{} out of {}'.format(count, len(lines)))
       if limit != -1 and count > limit:
           break
 
@@ -35,16 +55,24 @@ def load_lfw_data(image_dir, attributes_path, input_shape = (224, 224, 3), rando
   attribtes = np.array(attributes)
   return (faces, attributes, labels)
 
-def split_data_by_ratio(x, y, split=.2):
-    num_samples = len(x)
-    num_train_samples = int((1 - split)*num_samples)
-    train_x = x[:num_train_samples]
-    train_y = y[:num_train_samples]
-    val_x = x[num_train_samples:]
-    val_y = y[num_train_samples:]
-    train_data = (train_x, train_y)
-    val_data = (val_x, val_y)
-    return train_data, val_data
+def split_data_by_ratio(x, y, split=[0.8, 0.1, 0.1]):
+  sum = 0
+  for ratio in split:
+    sum += ratio
+  assert sum == 1
+  num_samples = len(x)
+  results = []
+  start = 0
+  end_ratio = 0
+  for i in range(len(split)):
+    end_ratio += split[i]
+    end_samples = int(end_ratio * num_samples)
+    x_ = x[start:end_samples]
+    y_ = y[start:end_samples]
+    print(start, end_samples)
+    results.append([x_, y_])
+    start = end_samples
+  return results
 
 class MacOSFile(object):
     def __init__(self, f):
--- train_generator.py
+++ train_generator.py
@@ -13,17 +13,18 @@ from keras.preprocessing.image import ImageDataGenerator
 
 from keras.backend.tensorflow_backend import set_session
 import tensorflow as tf
-# config = tf.ConfigProto()
+config = tf.ConfigProto()
 # config.gpu_options.allocator_type = 'BFC'
 # config.gpu_options.per_process_gpu_memory_fraction = 0.5
-# set_session(tf.Session(config=config))
+set_session(tf.Session(config=config))
+
 
 import datetime
 import os
 import pickle
 import numpy as np
 
-from data import load_data, split_data_by_ratio
+from data import load_attributes
 from sys import platform
 if platform == 'darwin':
     from data import MacOSFile
@@ -35,7 +36,6 @@ alpha=1.0
 validation_split = .2
 verbose = 1
 patience = 50
-use_generator=True
 data_type = 'celeba'
 if data_type == 'lfw':
     data_dir = '/data/lfw'
@@ -43,56 +43,45 @@ if data_type == 'lfw':
     attributes_path = os.path.join(data_dir, 'lfw_header_lines_45.p')
 elif data_type == 'celeba':
     data_dir = '/data/celeba'
-    images_path = os.path.join(data_dir, 'Img/img_align_celeba/')
+    images_path = os.path.join(data_dir, 'Img/img_align_celeba_crop_middle/')
+    train_images_path = os.path.join(data_dir, 'Img/img_align_celeba_crop_middle_train/')
+    val_images_path = os.path.join(data_dir, 'Img/img_align_celeba_crop_middle_val/')
+    test_images_path = os.path.join(data_dir, 'Img/img_align_celeba_crop_middle_test/')
     attributes_path = os.path.join('.', 'celeba_header_lines_40.p')
 else:
     assert False, data_type + ' not supported'
 
-local_data_path = './data'
-limit=-1
-
-if not os.path.isdir(local_data_path):
-    os.makedirs(local_data_path)
-data_file = os.path.join(local_data_path,
-                         '{}_{}_{}_{}_limit_{}.p'.format(data_type, input_shape[0], input_shape[1], input_shape[2], limit))
-
-if not os.path.isfile(data_file):
-    faces, attributes, labels = load_data(images_path, attributes_path, input_shape, random=False, limit=limit)
-    attributes = np.array(attributes)
-    try:
-        if platform == 'darwin':
-            fo = MacOSFile(open(data_file, 'wb'))
-        else:
-            fo = open(data_file, 'wb')
-        pickle.dump({'faces': faces, 'attributes': attributes, 'labels': labels},
-                    fo, protocol=4)
-    except:
-        print('somehow writing to pickle failed')
-else:
-    if platform == 'darwin':
-        fi = MacOSFile(open(data_file, 'rb'))
-    else:
-        fi = open(data_file, 'rb')
-    data = pickle.load(fi)
-    faces = data['faces']
-    attributes = data['attributes']
-    labels = data['labels']
-
-# splitted_data = split_data_by_ratio(faces, attributes, [0.803409, 0.0980663281, 0.0985246719])
-splitted_data = split_data_by_ratio(faces, attributes, [0.8, 0.1, 0.1])
-assert False
-train_images, train_vals = splitted_data[0]
-val_data = splitted_data[1]
-test_data = splitted_data[2]
+def preprocess_image(image):
+  image = image / 255.0
+  image = image - 0.5
+  image = image * 2.0
+  return image
 
 # data generator
-data_generator = ImageDataGenerator(featurewise_center=False,
-                                    featurewise_std_normalization=False,
-                                    rotation_range=10,
-                                    width_shift_range=0.1,
-                                    height_shift_range=0.1,
-                                    zoom_range=.1,
-                                    horizontal_flip=True)
+train_datagen = ImageDataGenerator(rotation_range=10,
+                                   width_shift_range=0.1,
+                                   height_shift_range=0.1,
+                                   zoom_range=.1,
+                                   horizontal_flip=True,
+                                   preprocessing_function=preprocess_image)
+val_datagen = ImageDataGenerator(preprocessing_function=preprocess_image)
+multilabel_dict, labels = load_attributes(attributes_path)
+train_generator = train_datagen.flow_from_directory(
+    train_images_path,
+    target_size=(input_shape[1], input_shape[0]),
+    batch_size=batch_size,
+    classes=labels,
+    class_mode='multilabel',
+    multilabel_classes=multilabel_dict,
+)
+val_generator = val_datagen.flow_from_directory(
+    val_images_path,
+    target_size=(input_shape[1], input_shape[0]),
+    batch_size=batch_size,
+    classes=labels,
+    class_mode='multilabel',
+    multilabel_classes=multilabel_dict,
+)
 
 now = datetime.datetime.now()
 model_name = 'mobilenet_{}_{}'.format(alpha, input_shape[0])
@@ -100,7 +89,6 @@ base_path = 'models/' + model_name + '_'
 base_path += now.strftime("%Y_%m_%d_%H_%M_%S") + '/'
 if not os.path.exists(base_path):
     os.makedirs(base_path)
-
 
 mobilenet_model = MobileNet(input_shape=input_shape, alpha=alpha, depth_multiplier=1, dropout=1e-3, include_top=False,
                             weights='imagenet', input_tensor=None, pooling=None, classes=len(labels))
@@ -132,20 +120,9 @@ model_checkpoint = ModelCheckpoint(model_names, 'val_acc', verbose=1,
                                    save_best_only=True)
 callbacks = [model_checkpoint, csv_logger, early_stop, reduce_lr]
 
-if use_generator:
-    model.fit_generator(data_generator.flow(train_images, train_vals, batch_size),
-                        steps_per_epoch = len(train_images)/batch_size,
-                        epochs = num_epochs,
-                        callbacks = callbacks,
-                        verbose = 1,
-                        validation_data = val_data
-    )
-else:
-    model.fit(x = train_images,
-              y = train_vals,
-              batch_size = batch_size,
-              epochs = num_epochs,
-              callbacks = callbacks,
-              verbose = 1,
-              validation_data = val_data
-    )
+model.fit_generator(train_generator,
+                    steps_per_epoch = 2000,
+                    epochs = num_epochs,
+                    callbacks = callbacks,
+                    verbose = 1,
+                    validation_data = val_generator)
